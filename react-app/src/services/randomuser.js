68958813daa963b178e35945168a1c459de8f952
Bhups P <92128+bhupesh@users.noreply.github.com>
2022-08-03 21:06:02 +0530

    Added reduxtoolkit and redux packages

--- react-app/src/services/randomuser.js
+++ react-app/src/services/randomuser.js
@@ -1,4 +1,4 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import {
   API_URL, noOfUsers, seed, nationalities
 } from '../config/config';
